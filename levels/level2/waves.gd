dfce4a1748a8c9a507eda588a41bbb134cd2f4c2
Hunter Baker <bakerhb@dukes.jmu.edu>
2025-07-20 10:54:16 -0400

    wave changes

--- levels/level2/waves.gd
+++ levels/level2/waves.gd
@@ -5,26 +5,73 @@ var waves
 const SOLDIER = "res://enemies/base-enemy/BaseEnemy.tscn"
 const BEEFYSOLDIER = "res://enemies/beefy-enemy/BeefyEnemy.tscn"
 const TANK = "res://enemies/tank-enemy/TankEnemy.tscn"
+const RAT = "res://enemies/rat-enemy/RatEnemy.tscn"
 
 func _init() -> void:
 	self.waves = {
 		1 : [
-			[TANK, 1],
 			[SOLDIER, 4],
 			[BEEFYSOLDIER, 1],
-			[SOLDIER, 4],
-			[BEEFYSOLDIER, 1],
-			[SOLDIER, 4],
-			[SOLDIER, 4],
 		],
 		2 : [
-			[SOLDIER, 16],
-			[BEEFYSOLDIER, 8],
-			[TANK, 2]
+			[BEEFYSOLDIER, 1],
+			[SOLDIER, 4],
+			[BEEFYSOLDIER, 1],
+			[SOLDIER, 2],
+			[RAT, 8]
 		],
 		3 : [
+			[BEEFYSOLDIER, 4],
+			[SOLDIER, 16],
+			[BEEFYSOLDIER, 2],
+			[SOLDIER, 8],
+			[RAT, 15]
+		],
+		4 : [
+			[TANK, 1],
+			[SOLDIER, 20],
+			[BEEFYSOLDIER, 6],
 			[SOLDIER, 32],
-			[BEEFYSOLDIER, 16],
-			[TANK, 4]
+			[RAT, 50]
+		],
+		5 : [
+			[TANK, 2],
+			[BEEFYSOLDIER, 32],
+			[TANK, 1],
+			[BEEFYSOLDIER, 50],
+			[RAT, 50],
+		],
+		6 : [
+			[SOLDIER, 100],
+			[RAT, 50],
+			[SOLDIER, 100],
+			[RAT, 50],
+			#[SOLDIER, 100],
+			#[RAT, 50],
+			#[SOLDIER, 100],
+			#[RAT, 50],
+			#[SOLDIER, 100],
+			#[RAT, 50],
+			#[SOLDIER, 100],
+			#[RAT, 50],
+		],
+		7 : [
+			[RAT, 24],
+			[RAT, 100],
+			[TANK, 32],
+			[RAT, 100],
+			[RAT, 100],
+			[RAT, 100],
+			#[RAT, 100]
+		],
+		8 : [
+			[TANK, 8],
+			[SOLDIER, 64],
+			[TANK, 16],
+			[SOLDIER, 64],
+			[RAT, 150],
+			[BEEFYSOLDIER, 32],
+			[SOLDIER, 64],
+			[RAT, 150],
 		]
 	}
--- levels/level3/waves.gd
+++ levels/level3/waves.gd
@@ -5,26 +5,48 @@ var waves
 const SOLDIER = "res://enemies/base-enemy/BaseEnemy.tscn"
 const BEEFYSOLDIER = "res://enemies/beefy-enemy/BeefyEnemy.tscn"
 const TANK = "res://enemies/tank-enemy/TankEnemy.tscn"
+const RAT = "res://enemies/rat-enemy/RatEnemy.tscn"
 
 func _init() -> void:
 	self.waves = {
 		1 : [
-			[TANK, 1],
-			[SOLDIER, 4],
-			[BEEFYSOLDIER, 1],
-			[SOLDIER, 4],
-			[BEEFYSOLDIER, 1],
-			[SOLDIER, 4],
-			[SOLDIER, 4],
+			[RAT, 30],
 		],
 		2 : [
-			[SOLDIER, 16],
-			[BEEFYSOLDIER, 8],
-			[TANK, 2]
+			[SOLDIER, 30]
 		],
 		3 : [
-			[SOLDIER, 32],
-			[BEEFYSOLDIER, 16],
-			[TANK, 4]
+			[BEEFYSOLDIER, 30]
+		],
+		4 : [
+			[BEEFYSOLDIER, 30],
+			[SOLDIER, 30],
+			[RAT, 30],
+		],
+		5 : [
+			[TANK, 15]
+		],
+		6 : [
+			[SOLDIER, 100],
+			[RAT, 50],
+			[SOLDIER, 100],
+			[RAT, 50],
+			[SOLDIER, 100],
+			[RAT, 50],
+		],
+		7 : [
+			[RAT, 24],
+			[RAT, 100],
+			[TANK, 32],
+			[RAT, 100],
+			[RAT, 100],
+			[RAT, 100],
+			[RAT, 100]
+		],
+		8 : [
+			[TANK, 100],
+			[BEEFYSOLDIER, 250],
+			[SOLDIER, 500],
+			[RAT, 1000],
 		]
 	}
