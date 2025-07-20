4e7abadf414368a0f2422148e1b1eecb0d41c7dc
Hunter Baker <bakerhb@dukes.jmu.edu>
2025-07-20 02:32:22 -0400

    little guys look the right way

--- enemies/base-enemy/base_enemy.gd
+++ enemies/base-enemy/base_enemy.gd
@@ -11,14 +11,27 @@ class_name BaseEnemy
 @export var speed : int = 100
 @export var damage : int = 1 # how much to hurt nexus
 
-
+var previousPosition: Vector2
 
 func _ready() -> void:
+	rotation = 0
 	timer.wait_time = 1.0 / attacksPerSecond
 	timer.timeout.connect($StateMachine/Attack.onTimerTimout)
 	stateMachine.custom_init(self)
 	healthComponent.died.connect(onDeath)
 	hitboxComponent.damage = damage
+
+func _process(delta: float) -> void:
+	var sprite = $Sprite2D
+	global_rotation = 0 # ensure that we're upward
+	
+	var movement = global_position - previousPosition
+	if movement.x < 0:
+		sprite.flip_h = true   # sprite looks left
+	elif movement.x > 0:
+		sprite.flip_h = false  # looks right
+	
+	previousPosition = global_position
 
 func onDeath():
 	stateMachine.change_state(stateMachine.current_state, "dead")
