da3918c6cfa848f2048ce16fa32f59266d21c049
Jakob Bennie <jayfeather874@gmail.com>
2025-07-20 10:50:34 -0400

    Basic towers aim at closest to core now

--- objects/buildings/gunner-building/states/active.gd
+++ objects/buildings/gunner-building/states/active.gd
@@ -15,12 +15,20 @@ func enemiesInRange():
 			enemies.append(areaParent)
 	return enemies
 
+func getEnemyClosestToCore(enemies: Array):
+	var highestProgressRatio = enemies[0]
+	for enemy in enemies:
+		var path = enemy.get_parent() as PathFollow2D
+		if path.progress_ratio > highestProgressRatio.get_parent().progress_ratio:
+			highestProgressRatio = enemy
+	return highestProgressRatio
+
 func aim():
 	var enemies = enemiesInRange()
 	if !enemies:
 		transitioned.emit(self, "idle")
 		return
-	var enemyToShoot = enemies[0]
+	var enemyToShoot = getEnemyClosestToCore(enemies)
 	var direction = enemyToShoot.global_position - parent.global_position
 	var angleRadians = direction.angle()
 	parent.gun.shoot(angleRadians, direction)
